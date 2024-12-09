12658b728f4b0b7bd35735314575ab2ab7ea91f5
lomticksu <lomticksu@yandex.ru>
2024-12-10 01:26:21 +0300

    Added translation to Application

--- src/Application.py
+++ src/Application.py
@@ -1,4 +1,5 @@
 import sys
+from PyQt5.QtCore import QTranslator
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtSql import QSqlDatabase
 import settings as st
@@ -9,6 +10,23 @@ class Application(QApplication):
     def __init__(self, argv):
         super().__init__(argv)
         
+        self.setup()
+    
+    def setup(self):
+        self.connect_to_db()
+        self.set_translation()
+        return self
+    
+    def set_translation(self):
+        trans = QTranslator(parent=self)
+        ok = trans.load('airport_ru.qm')
+        if ok:
+            print('Translation loaded', file=sys.stderr)
+        else:
+            print('FAILED to load translation', file=sys.stderr)
+        QApplication.installTranslator(trans)
+    
+    def connect_to_db(self):
         db = QSqlDatabase.addDatabase('QPSQL')
         db.setHostName(st.db_params['host'])
         db.setDatabaseName(st.db_params['dbname'])
