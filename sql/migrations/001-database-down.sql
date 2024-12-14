fc65507a10f09b5312dcb89792a83ccc85c39d6f
lomticksu <lomticksu@yandex.ru>
2024-12-14 21:27:56 +0300

    Added new functions

--- sql/migrations/001-database-down.sql
+++ sql/migrations/001-database-down.sql
@@ -2,17 +2,38 @@ START TRANSACTION;
 
 /*----------------------------------------------------------------------------------------------------------*/
 
-DROP TRIGGER 	IF EXISTS update_price ON Ticket;
+DROP FUNCTION 	IF EXISTS ticket_insert;
+DROP FUNCTION 	IF EXISTS ticket_select_one;
+DROP FUNCTION 	IF EXISTS ticket_select;
+
+DROP FUNCTION 	IF EXISTS flight_insert;
+DROP FUNCTION 	IF EXISTS flight_select_one;
+DROP FUNCTION 	IF EXISTS flight_select;
+
+DROP FUNCTION 	IF EXISTS plane_insert;
+DROP FUNCTION 	IF EXISTS plane_select_one;
+DROP FUNCTION 	IF EXISTS plane_select;
+
+DROP FUNCTION 	IF EXISTS airline_insert;
+DROP FUNCTION 	IF EXISTS airline_select_one;
+DROP FUNCTION 	IF EXISTS airline_select;
+
+DROP FUNCTION 	IF EXISTS airport_search;
+DROP FUNCTION 	IF EXISTS airport_insert;
+DROP FUNCTION 	IF EXISTS airport_select_one;
+DROP FUNCTION 	IF EXISTS airport_select;
+
+DROP TRIGGER 	IF EXISTS update_price ON ticket;
 
 DROP FUNCTION 	IF EXISTS can_register_passenger;
 DROP FUNCTION 	IF EXISTS calculate_price;
 
-DROP TABLE 		IF EXISTS Ticket;
-DROP TABLE 		IF EXISTS Flight;
-DROP TABLE 		IF EXISTS Plane;
-DROP TABLE 		IF EXISTS Airline;
+DROP TABLE 		IF EXISTS ticket;
+DROP TABLE 		IF EXISTS flight;
+DROP TABLE 		IF EXISTS plane;
+DROP TABLE 		IF EXISTS airline;
 DROP INDEX 		IF EXISTS idx_airport_name;
-DROP TABLE 		IF EXISTS Airport;
+DROP TABLE 		IF EXISTS airport;
 
 DROP EXTENSION 	IF EXISTS pg_trgm;
 
--- sql/migrations/001-database-up.sql
+++ sql/migrations/001-database-up.sql
@@ -7,97 +7,97 @@ CREATE EXTENSION pg_trgm;
 /*----------------------------------------------------------------------------------------------------------*/
 
 
-CREATE TABLE Airport (
-	AirportID 	SERIAL PRIMARY KEY,
-	AirportName VARCHAR(100) NOT NULL,
-	City 		VARCHAR(100) NOT NULL
+CREATE TABLE airport (
+	airportid 	SERIAL PRIMARY KEY,
+	airportname VARCHAR(100) NOT NULL,
+	city 		VARCHAR(100) NOT NULL
 );
 
-COMMENT ON TABLE Airport IS 'Сведения об аэропортах';
-COMMENT ON COLUMN Airport.AirportID	IS 'название аэропорта';
-COMMENT ON COLUMN Airport.AirportID	IS 'город, в котором находится аэропорт';
+COMMENT ON TABLE airport IS 'Сведения об аэропортах';
+COMMENT ON COLUMN Airport.airportname	IS 'название аэропорта';
+COMMENT ON COLUMN Airport.city			IS 'город, в котором находится аэропорт';
 
 /*----------------------------------------------------------------------------------------------------------*/
 
-CREATE INDEX idx_airport_name ON Airport USING gist (AirportName gist_trgm_ops);
+CREATE INDEX idx_airport_name ON airport USING gist (airportname gist_trgm_ops);
 
-COMMENT ON INDEX idx_airport_name IS 'Индекс по текстовому не ключевому полю AirportName в таблице Airport';
-
-/*----------------------------------------------------------------------------------------------------------*/
-
-CREATE TABLE Airline (
-	AirlineID 	SERIAL PRIMARY KEY,
-	AirlineName VARCHAR(100) 	NOT NULL,
-	IATACode 	VARCHAR(3)		NOT NULL UNIQUE
-);
-
-COMMENT ON TABLE Airline IS 'Сведения об авиакомпаниях';
-COMMENT ON COLUMN Airline.AirlineName	IS 'название авиакомпании';
-COMMENT ON COLUMN Airline.IATACode		IS 'уникальный код авиакомпании';
+COMMENT ON INDEX idx_airport_name IS 'Индекс по текстовому не ключевому полю AirportName в таблице airport';
 
 /*----------------------------------------------------------------------------------------------------------*/
 
-CREATE TABLE Plane (
-	PlaneID 	SERIAL PRIMARY KEY,
-	AirlineID 	INT 			REFERENCES Airline(AirlineID) ON DELETE CASCADE,
-	Model 		VARCHAR(100) 	NOT NULL,
-	Capacity 	INT 			NOT NULL CHECK (Capacity > 0)
+CREATE TABLE airline (
+	airlineid 	SERIAL PRIMARY KEY,
+	airlinename VARCHAR(100) 	NOT NULL,
+	iatacode 	VARCHAR(3)		NOT NULL UNIQUE
 );
 
-COMMENT ON TABLE Plane IS 'Сведения о самолетах';
-COMMENT ON COLUMN Plane.AirlineID	IS 'id авиакомпании, которой принадлежит самолет';
-COMMENT ON COLUMN Plane.Model		IS 'модель самолета';
-COMMENT ON COLUMN Plane.Capacity	IS 'вместимость самолета (чел.)';
+COMMENT ON TABLE airline IS 'Сведения об авиакомпаниях';
+COMMENT ON COLUMN airline.airlinename	IS 'название авиакомпании';
+COMMENT ON COLUMN airline.iatacode		IS 'уникальный код авиакомпании';
 
 /*----------------------------------------------------------------------------------------------------------*/
 
-CREATE TABLE Flight (
-    FlightID 			SERIAL PRIMARY KEY,                         
-    PlaneID 			INT 		REFERENCES Plane(PlaneID) ON DELETE CASCADE, 
-    DepartureAirportID 	INT 		REFERENCES Airport(AirportID) ON DELETE CASCADE, 
-    ArrivalAirportID 	INT 		REFERENCES Airport(AirportID) ON DELETE CASCADE,   
-    FlightTime 			TIMESTAMP 	NOT NULL,
-    Duration 			INTERVAL 	NOT NULL,
-    BaseTicketPrice 	INT 		NOT NULL CHECK (BaseTicketPrice > 0)
+CREATE TABLE plane (
+	planeid 	SERIAL PRIMARY KEY,
+	airlineid 	INT 			REFERENCES airline(airlineid) ON DELETE CASCADE,
+	model 		VARCHAR(100) 	NOT NULL,
+	capacity 	INT 			NOT NULL CHECK (capacity > 0)
 );
 
-COMMENT ON TABLE Flight IS 'Сведения о рейсах';
-COMMENT ON COLUMN Flight.PlaneID			IS 'id самолета';
-COMMENT ON COLUMN Flight.DepartureAirportID	IS 'id аэропорта вылета';
-COMMENT ON COLUMN Flight.ArrivalAirportID	IS 'id аэропорта прилета';
-COMMENT ON COLUMN Flight.FlightTime			IS 'время вылета';
-COMMENT ON COLUMN Flight.Duration			IS 'время в пути';
-COMMENT ON COLUMN Flight.BaseTicketPrice	IS 'базовая стоимость билета (без учета обеда на борту)';
+COMMENT ON TABLE plane IS 'Сведения о самолетах';
+COMMENT ON COLUMN plane.airlineid	IS 'id авиакомпании, которой принадлежит самолет';
+COMMENT ON COLUMN plane.model		IS 'модель самолета';
+COMMENT ON COLUMN plane.capacity	IS 'вместимость самолета (чел.)';
 
 /*----------------------------------------------------------------------------------------------------------*/
 
-CREATE TABLE Ticket (
-    TicketID 		SERIAL PRIMARY KEY,               
-    FlightID 		INT 			REFERENCES Flight(FlightID) ON DELETE CASCADE,  
-    FullName 		VARCHAR(255) 	NOT NULL,              
-    PassportNumber 	VARCHAR(10) 	NOT NULL,         
-    SeatNumber 		VARCHAR(3) 		NOT NULL,                            
-    Meal 			VARCHAR(3) 		CHECK (Meal IN ('YES', 'NO')),
-	Price 			INT 			NOT NULL
+CREATE TABLE flight (
+    flightid 			SERIAL PRIMARY KEY,                         
+    planeid 			INT 		REFERENCES plane(planeid) ON DELETE CASCADE, 
+    departureairportID 	INT 		REFERENCES airport(airportid) ON DELETE CASCADE, 
+    arrivalairportID 	INT 		REFERENCES airport(airportid) ON DELETE CASCADE,   
+    flighttime 			TIMESTAMP 	NOT NULL,
+    duration 			INTERVAL 	NOT NULL,
+    baseticketprice 	INT 		NOT NULL CHECK (baseticketprice > 0)
 );
 
-COMMENT ON TABLE Ticket IS 'Сведения о билетах';
-COMMENT ON COLUMN Ticket.FlightID		IS 'id рейса';
-COMMENT ON COLUMN Ticket.FullName		IS 'ФИО пассажира';
-COMMENT ON COLUMN Ticket.PassportNumber	IS 'серия и номер паспорта пассажира';
-COMMENT ON COLUMN Ticket.SeatNumber		IS 'номер места в самолете';
-COMMENT ON COLUMN Ticket.Meal			IS 'обед на борту (да/нет)';
-COMMENT ON COLUMN Ticket.Price			IS 'итоговая стоимость билета (заполняется/изменяется триггером)';
+COMMENT ON TABLE flight IS 'Сведения о рейсах';
+COMMENT ON COLUMN flight.planeid			IS 'id самолета';
+COMMENT ON COLUMN flight.departureairportid	IS 'id аэропорта вылета';
+COMMENT ON COLUMN flight.arrivalairportid	IS 'id аэропорта прилета';
+COMMENT ON COLUMN flight.flighttime			IS 'время вылета';
+COMMENT ON COLUMN flight.duration			IS 'время в пути';
+COMMENT ON COLUMN flight.baseticketprice	IS 'базовая стоимость билета (без учета обеда на борту)';
+
+/*----------------------------------------------------------------------------------------------------------*/
+
+CREATE TABLE ticket (
+    ticketID 		SERIAL PRIMARY KEY,               
+    flightID 		INT 			REFERENCES flight(flightid) ON DELETE CASCADE,  
+    fullname 		VARCHAR(255) 	NOT NULL,              
+    passportnumber 	VARCHAR(10) 	NOT NULL,         
+    seatnumber 		VARCHAR(3) 		NOT NULL,                            
+    meal 			VARCHAR(3) 		CHECK (meal IN ('YES', 'NO')),
+	price 			INT 			NOT NULL
+);
+
+COMMENT ON TABLE ticket IS 'Сведения о билетах';
+COMMENT ON COLUMN ticket.flightid		IS 'id рейса';
+COMMENT ON COLUMN ticket.fullname		IS 'ФИО пассажира';
+COMMENT ON COLUMN ticket.passportnumber	IS 'серия и номер паспорта пассажира';
+COMMENT ON COLUMN ticket.seatnumber		IS 'номер места в самолете';
+COMMENT ON COLUMN ticket.meal			IS 'обед на борту (да/нет)';
+COMMENT ON COLUMN ticket.price			IS 'итоговая стоимость билета (заполняется/изменяется триггером)';
 
 /*----------------------------------------------------------------------------------------------------------*/
 
 CREATE OR REPLACE FUNCTION calculate_price()
 RETURNS TRIGGER AS $$
 BEGIN
-	IF NEW.Meal = 'NO' 		THEN
-        NEW.Price := (SELECT BaseTicketPrice FROM Flight WHERE FlightID = NEW.FlightID);
-    ELSIF NEW.Meal = 'YES' 	THEN
-        NEW.Price := (SELECT BaseTicketPrice FROM Flight WHERE FlightID = NEW.FlightID) + 499;
+	IF NEW.meal = 'NO' 		THEN
+        NEW.price := (SELECT baseticketprice FROM flight WHERE flightid = NEW.flightid);
+    ELSIF NEW.meal = 'YES' 	THEN
+        NEW.price := (SELECT baseticketprice FROM flight WHERE Flightid = NEW.flightid) + 499;
 	END IF;
     RETURN NEW;
 END;
@@ -115,12 +115,12 @@ DECLARE
     plane_capacity INT;
 BEGIN
     SELECT COUNT(*) INTO occupied_seats
-    FROM Ticket
-    WHERE FlightID = flight_id;
-    SELECT Capacity INTO plane_capacity
-    FROM Plane 
-    JOIN Flight ON Plane.PlaneID = Flight.PlaneID
-    WHERE Flight.FlightID = flight_id;
+    FROM ticket
+    WHERE flightid = flight_id;
+    SELECT capacity INTO plane_capacity
+    FROM plane 
+    JOIN flight ON plane.planeid = flight.planeid
+    WHERE flight.flightid = flight_id;
     IF occupied_seats < plane_capacity THEN
         RETURN TRUE;  -- Можно 
     ELSE
@@ -134,11 +134,283 @@ COMMENT ON FUNCTION can_register_passenger IS 'Проверка возможно
 /*----------------------------------------------------------------------------------------------------------*/
 
 CREATE TRIGGER update_price
-BEFORE INSERT OR UPDATE ON Ticket
+BEFORE INSERT OR UPDATE ON ticket
 FOR EACH ROW
 EXECUTE FUNCTION calculate_price();
 
-COMMENT ON TRIGGER update_price ON Ticket IS 'Триггер на таблицу со сведениями о билетах с применением функции подсчета итоговой стоимости билета';
+COMMENT ON TRIGGER update_price ON ticket IS 'Триггер на таблицу со сведениями о билетах с применением функции подсчета итоговой стоимости билета';
+
+/*----------------------------------------------------------------------------------------------------------*/
+
+CREATE OR REPLACE FUNCTION airport_select()
+RETURNS TABLE(
+	aid INT, 
+	nam VARCHAR(100),
+	cit VARCHAR(100)
+) AS $$
+BEGIN
+	RETURN QUERY 
+	SELECT airportid, airportname, city 
+	FROM airport;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION airport_select_one(airp_id INT)
+RETURNS TABLE(
+	nam VARCHAR(100),
+	cit VARCHAR(100)
+) AS $$
+BEGIN
+	RETURN QUERY
+	SELECT airportname, city 
+	FROM airport 
+	WHERE airportid = airp_id;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION airport_insert(
+	airp_name VARCHAR(100), 
+	airp_city VARCHAR(100)
+)
+RETURNS INT AS $$
+DECLARE airp_id INT;
+BEGIN
+    INSERT INTO airport ( airportname, city )
+    VALUES ( airp_name, airp_city )
+    RETURNING airportid INTO airp_id;
+    RETURN airp_id;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION airport_search(airp_name VARCHAR(100))
+RETURNS TABLE(
+	nam VARCHAR(100),
+	sim REAL
+) AS $$
+BEGIN
+	RETURN QUERY
+	SELECT airportname, similarity(airportname, $1) AS similarity 
+	FROM airport 
+	WHERE airportname % $1
+	ORDER BY similarity DESC;
+END;
+$$ LANGUAGE plpgsql;
+
+/*----------------------------------------------------------------------------------------------------------*/
+
+CREATE OR REPLACE FUNCTION airline_select()
+RETURNS TABLE(
+	aid INT, 
+	nam VARCHAR(100),
+	cod VARCHAR(3)
+) AS $$
+BEGIN
+	RETURN QUERY 
+	SELECT airlineid, airlinename, iatacode
+	FROM airline;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION airline_select_one(airl_id INT)
+RETURNS TABLE(
+	nam VARCHAR(100),
+	cod VARCHAR(3)
+) AS $$
+BEGIN
+	RETURN QUERY
+	SELECT airlinename, iatacode
+	FROM airline 
+	WHERE airlineid = airl_id;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION airline_insert(
+	airl_name VARCHAR(100), 
+	airl_code VARCHAR(3)
+)
+RETURNS INT AS $$
+DECLARE airl_id INT;
+BEGIN
+	INSERT INTO airline ( airlinename, iatacode )
+    VALUES ( airl_name, airl_code )
+    RETURNING airlineId INTO airl_id;
+    RETURN airl_id;
+END;
+$$ LANGUAGE plpgsql;
+
+/*----------------------------------------------------------------------------------------------------------*/
+
+CREATE OR REPLACE FUNCTION plane_select()
+RETURNS TABLE(
+	pid INT,
+	aid INT, 
+	mod VARCHAR(100),
+	cap INT
+) AS $$
+BEGIN
+	RETURN QUERY 
+	SELECT planeid, airlineid, model, capacity
+	FROM plane;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION plane_select_one(p_id INT)
+RETURNS TABLE(
+	aid INT, 
+	mod VARCHAR(100),
+	cap INT
+) AS $$
+BEGIN
+	RETURN QUERY
+	SELECT airlineid, model, capacity
+	FROM plane 
+	WHERE planeid = p_id;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION plane_insert(
+	airl_id INT, 
+	p_model VARCHAR(100), 
+	p_capacity INT
+)
+RETURNS INT AS $$
+DECLARE p_id INT;
+BEGIN
+	INSERT INTO plane ( airlineid, model, capacity )
+    VALUES ( airl_id, p_model, p_capacity )
+    RETURNING planeid INTO p_id;
+    RETURN p_id;
+END;
+$$ LANGUAGE plpgsql;
+
+/*----------------------------------------------------------------------------------------------------------*/
+
+CREATE OR REPLACE FUNCTION flight_select()
+RETURNS TABLE(
+	fid INT,
+	pid INT, 
+	daid INT, 
+	aaid INT,
+	tim TIMESTAMP,
+	dur INTERVAL,
+	btp INT
+) AS $$
+BEGIN
+	RETURN QUERY 
+	SELECT flightid, planeid, departureairportid, arrivalairportid, flighttime, duration, baseticketprice
+	FROM flight;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION flight_select_one(f_id INT)
+RETURNS TABLE(
+	pid INT, 
+	daid INT, 
+	aaid INT,
+	tim TIMESTAMP,
+	dur INTERVAL,
+	btp INT
+) AS $$
+BEGIN
+	RETURN QUERY
+	SELECT planeid, departureairportid, arrivalairportid, flighttime, duration, baseticketprice
+	FROM flight 
+	WHERE flightid = f_id;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION flight_insert(
+	p_id INT, 
+	dairp_id INT, 
+	aairp_id INT, 
+	f_time TIMESTAMP, 
+	f_duration INTERVAL, 
+	f_bprice INT
+)
+RETURNS INT AS $$
+DECLARE f_id INT;
+BEGIN
+	INSERT INTO flight ( planeid, departureairportid, arrivalairportid, flighttime, duration, baseticketprice )
+    VALUES ( p_id, dairp_id, aairp_id, f_time,  f_duration, f_bprice)
+    RETURNING flightid INTO f_id;
+    RETURN f_id;
+END;
+$$ LANGUAGE plpgsql;
+
+/*----------------------------------------------------------------------------------------------------------*/
+
+CREATE OR REPLACE FUNCTION ticket_select()
+RETURNS TABLE(
+	tid INT,
+	fid INT, 
+	fn VARCHAR(255), 
+	pn VARCHAR(10),
+	sn VARCHAR(3),
+	ml VARCHAR(3),
+	pr INT
+) AS $$
+BEGIN
+	RETURN QUERY 
+	SELECT ticketid, flightid, fullname, passportnumber, seatnumber, meal, price
+	FROM ticket;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION ticket_select_one(t_id INT)
+RETURNS TABLE(
+	fid INT, 
+	fn VARCHAR(255), 
+	pn VARCHAR(10),
+	sn VARCHAR(3),
+	ml VARCHAR(3),
+	pr INT
+) AS $$
+BEGIN
+	RETURN QUERY
+	SELECT flightid, fullmame, passportnumber, seatnumber, meal, price
+	FROM ticket 
+	WHERE ticketid = t_id;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION ticket_insert(
+	f_id INT, 
+	t_fname VARCHAR(255), 
+	t_passport VARCHAR(10), 
+	t_seat VARCHAR(3), 
+	t_meal VARCHAR(3)
+)
+RETURNS INT AS $$
+DECLARE t_id INT;
+BEGIN
+	INSERT INTO ticket ( flightid, fullname, passportnumber, seatnumber, meal )
+    VALUES ( f_id, t_fname, t_passport, t_seat, t_meal )
+    RETURNING ticketid INTO t_id;
+    RETURN t_id;
+END;
+$$ LANGUAGE plpgsql;
 
 /*----------------------------------------------------------------------------------------------------------*/
 
