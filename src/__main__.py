8a9ac97e663a7a50005aa8e6bc0a61b7eb06d76d
lomticksu <lomticksu@yandex.ru>
2024-12-15 14:49:36 +0300

    Added creating database

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,32 @@
+import os
 import sys
 from Application import Application
 from MainWindow import MainWindow
 
+import psycopg2
+import settings as st
 
-app = Application(sys.argv)
 
-main_window = MainWindow()
-main_window.showMaximized()
+def createdb():
+    os.system('./createdb_script.sh')
 
-result = app.exec()
-sys.exit(result)
+
+def main():
+    try:
+        psycopg2.connect(**st.db_params)
+        print(f'''Database "{st.db_params['dbname']}" exists''', file=sys.stderr)
+    except psycopg2.OperationalError:
+        createdb()
+        print(f'''Created database "{st.db_params['dbname']}"''', file=sys.stderr)
+    
+    app = Application(sys.argv)
+
+    main_window = MainWindow()
+    main_window.showMaximized()
+
+    result = app.exec()
+    sys.exit(result)
+
+
+if __name__ == '__main__':
+    main()
